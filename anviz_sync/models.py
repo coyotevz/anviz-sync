822ee1fcf27ba5c821c76debb89d58cc0692487a
Augusto Roccasalva <augusto@rioplomo.com.ar>
2022-09-05 08:34:09 -0300

    Adding rt module, split models to exclusive module

---
+++ anviz_sync/models.py
@@ -0,0 +1,18 @@
+# -*- coding=utf-8 -*-
+
+from anviz_sync.saw import SQLAlchemy
+
+db = SQLAlchemy()
+
+class AttendanceRecord(db.Model):
+    __tablename__ = 'attendance_record'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_code = db.Column(db.Integer, nullable=False)
+    datetime = db.Column(db.DateTime, nullable=False, unique=True)
+    bkp_type = db.Column(db.Integer, nullable=False)
+    type_code = db.Column(db.Integer, nullable=False)
+
+
+def configure_db(db_uri):
+    db.configure(db_uri)
---
+++ anviz_sync/rt.py
@@ -0,0 +1,129 @@
+# -*- coding=utf-8 -*-
+
+"""
+    anviz-sync real-time
+    ~~~~~~~~~~~~~~~~~~~~
+
+    Software that listen on socket connection for Anviz A300 device request and store data in db.
+
+    :copyright: (c) 2022 by Augusto Roccasalva
+    :license: BSD, see LICENSE for more details.
+"""
+
+import socket
+import struct
+from configparser import ConfigParser
+from datetime import datetime
+
+from anviz_sync import anviz
+from anviz_sync.models import AttendanceRecord, configure_db, db
+from anviz_sync.saw import SQLAlchemy
+
+
+def get_record(raw_data):
+    crc_ok = anviz.crc16(raw_data[:23]) == raw_data[-2:]
+    if not crc_ok:
+        raise ValueError("CRC verificaton failed")
+    stx, dev_id, ack, ret, length = struct.unpack(">BLBBH", raw_data[:9])
+    record = anviz.parse_record(raw_data[9 : 9 + length])
+    return dev_id, record
+
+
+def show_data(dev_id, record):
+    time = datetime.now()
+    print(
+        f"> dev_id={dev_id} user_id={record.code} {record.datetime.isoformat()} bkp={record.bkp} {TYPES[record.type]} work={record.work} [{time}]"
+    )
+
+
+def main():
+    config = ConfigParser()
+    config.read("anviz-sync.ini")
+
+    # config device
+    ip_addr = config.get("anviz-rt", "ip_addr")
+    ip_port = config.get("anviz-rt", "ip_port")
+
+    reconnect = True
+
+    while reconnect:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind((ip_addr, ip_port))
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                time = datetime.now()
+                print(f"[{time}] Connected by {addr}")
+                try:
+                    while True:
+                        data = conn.recv(1024)
+                        if not data:
+                            break
+                        dev_id, record = get_record(data)
+                        show_data(dev_id, record)
+                except ConnectionResetError as err:
+                    time = datetime.now()
+                    print(f"[{time}] Closing socket, reconnecting...")
+                    pass
+                except socket.timeout as err:
+                    time = datetime.now()
+                    print(f"[{time}] Reconnecting ...")
+                    pass
+                except KeyboardInterrupt as err:
+                    reconnect = False
+                    print("Quit")
+
+
+#HOST = "192.168.1.100"
+#PORT = 5010
+#
+#TYPES = {
+#    0: "Entrada",
+#    1: "Salida",
+#    2: "BREAK",
+#}
+#
+#
+#def show_data_old(raw_data):
+#    crc_ok = anviz.crc16(raw_data[:23]) == raw_data[-2:]
+#    stx, dev_id, ack, ret, length = struct.unpack(">BLBBH", raw_data[:9])
+#    record = anviz.parse_record(raw_data[9 : 9 + length])
+#
+#    time = datetime.now()
+#    print(
+#        f"> dev_id={dev_id} user_id={record.code} {record.datetime.isoformat()} bkp={record.bkp} {TYPES[record.type]} work={record.work} crc: {crc_ok} [{time}]"
+#    )
+
+
+# reconnect = True
+#
+# while reconnect:
+#    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#        s.bind((HOST, PORT))
+#        s.listen()
+#        conn, addr = s.accept()
+#        with conn:
+#            time = datetime.now()
+#            print(f"[{time}] Connected by {addr}")
+#            try:
+#                while True:
+#                    data = conn.recv(1024)
+#                    if not data:
+#                        break
+#                    show_data(data)
+#            except ConnectionResetError as err:
+#                time = datetime.now()
+#                print(f"[{time}] Closing socket, reconnecting...")
+#                pass
+#            except socket.timeout as err:
+#                time = datetime.now()
+#                print(f"[{time}] Reconnecting ...")
+#                pass
+#            except KeyboardInterrupt as err:
+#                reconnect = False
+#                print("Quit")
+
+if __name__ == "__main__":
+    main()
